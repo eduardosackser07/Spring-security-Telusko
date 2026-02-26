0a64e220239cfae2ad2aeea8cd722d7b67d3ba8a
eduardosackser07 <martinseduardo261@gmail.com>
2026-02-26 18:31:20 -0300

    initial files

---
+++ src/main/java/com/github/eduardosackser07/duduzinhoGameplays/DuduzinhoGameplaysApplication.java
@@ -0,0 +1,13 @@
+package com.github.eduardosackser07.duduzinhoGameplays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuduzinhoGameplaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuduzinhoGameplaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/eduardosackser07/duduzinhoGameplays/DuduzinhoGameplaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eduardosackser07.duduzinhoGameplays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuduzinhoGameplaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
